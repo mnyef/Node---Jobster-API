3f2398f2382db13530f2bd3977efbfb224789a1d
mnyef <vladgav004@gmail.com>
2023-05-03 17:46:58 +0300

    Front-End Setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const path = require('path');
+
 // extra security packages
 const helmet = require('helmet');
 const xss = require('xss-clean');
@@ -17,6 +19,7 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -24,6 +27,10 @@ app.use(xss());
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
