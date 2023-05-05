22eab2d0cf1e6e4de681746d1eedd6c6d63da28d
mnyef <vladgav004@gmail.com>
2023-05-05 14:43:58 +0300

    Jobs Pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -34,8 +34,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
